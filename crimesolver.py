120d3a420971761806cb485e0d81608e7862efaa
Jenifer Nguyen <jwinn19464@gmail.com>
2023-12-05 06:59:14 -0500

    backtracking works

--- crimesolver.py
+++ crimesolver.py
@@ -1,24 +1,32 @@
-def find_possible_thieves():
-    suspects = ['A', 'B', 'C', 'D']
-    possible_thieves = []
+def find_thief(suspects, current_index, statements, truth_count, thief):
+    if current_index == len(suspects):
+        if truth_count == 1:
+            return True, thief
+        return False, None
+    
+    # Assume the current suspect is lying
+    result = find_thief(suspects, current_index + 1, statements, truth_count, thief)
+    if result[0]:
+        return result
+    
+    # If assuming the current suspect as a liar didn't lead to a solution, consider them telling the truth
+    truth_count += 1
+    result = find_thief(suspects, current_index + 1, statements, truth_count, suspects[current_index])
+    if result[0]:
+        return result
+    
+    return False, None
 
-    for thief in suspects:
-        statements = {
-            'A': (thief == 'B'),
-            'B': (thief != 'B'),
-            'C': (thief == 'D'),
-            'D': (thief == 'C')
-        }
+# Test scenario
+suspects = ['A', 'B', 'C']
+statements = [
+    "I am not the thief",
+    "A is the thief",
+    "I am not the thief"
+]
 
-        truthful_suspects = [suspect for suspect in statements if statements[suspect]]
-
-        if len(truthful_suspects) == 1:
-            possible_thieves.append(truthful_suspects[0])
-
-    return possible_thieves
-
-thieves = find_possible_thieves()
-if thieves:
-    print("Possible thief(s):", thieves)
+solution = find_thief(suspects, 0, statements, 0, None)
+if solution[0]:
+    print(f"The thief is {solution[1]}")
 else:
-    print("No unique solution found.")
+    print("No solution found")
