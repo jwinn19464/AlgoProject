78679474dad3ae1b3f2197e9849c55321136e209
Jenifer Nguyen <jwinn19464@gmail.com>
2023-12-05 20:56:26 -0500

    Final product with backtracking used

--- crimesolver.py
+++ crimesolver.py
@@ -1,9 +1,4 @@
-# suspects: a list containing the names of the suspects
-# current_index: index of current suspect being checked
-# statements: list containing statements each suspect made
-# truth_count: counter of how many suspects are telling the truth
-# thief: current assumed thief
-
+# Final Product
 def generate_statements(num_suspects):
     import random
     import names
@@ -20,7 +15,7 @@ def generate_statements(num_suspects):
     # Create statements based on shuffled suspects
     statements = []
     for i in range(num_suspects):
-        if i % 2 == 0:
+        if (i % 2 == 0) or (i % 3 == 0) or (i % 7 == 0):
             accusation = random.choice(suspects)
             while accusation == suspects[i]:  # Ensure a suspect doesn't accuse themselves
                 accusation = random.choice(suspects)
@@ -37,46 +32,70 @@ num_suspects = int(input("Enter the number of suspects: "))
 # Generate suspects and their statements
 suspects, statements = generate_statements(num_suspects)
 
+# print(statements)
 # Display the suspects and their statements
 for statement in statements:
     print(statement)
 
 # Print the list of suspects
-print("Suspects:", suspects)
+# print("Suspects:", suspects)
 
-def find_thief(suspects, current_index, statements, truth_count, thief):
-    # base case: when all suspects have been checked, see if conditions have been met to find thief
-    if current_index == len(suspects):
-        # potential solution if only 1 suspect is being truthful
-        if truth_count == 1:
-            return True, thief
-        return False, None
-    
-    # Assume the current suspect is lying and calls find_thief for next suspect
-    result = find_thief(suspects, current_index + 1, statements, truth_count, thief)
-    if result[0]:
-        return result
-    
-    # If assuming the current suspect as a liar didn't lead to a solution, consider them telling the truth
-    truth_count += 1
-    result = find_thief(suspects, current_index + 1, statements, truth_count, suspects[current_index])
-    if result[0]:
-        return result
-    
-    return False, None
+def parse_logic(statements):
+    truth_dict = {}  # Dictionary to store truth information
 
+    for statement in statements:
+        person, claim = statement.split(": ")
+        name, assertion = person.strip(), claim.strip().replace('"', '')
 
-solution = find_thief(suspects, 0, statements, 0, None)
-if solution[0]:
-    print(f"The thief is {solution[1]}")
+        if "the thief" in assertion:
+            subject = assertion.split("is the thief.")[0].strip()
+            if assertion.endswith("is the thief."):
+                truth_dict[name] = {"subject": subject, "isThief": True, "isNotThief": False, "t_count": 0}
+            else:
+                truth_dict[name] = {"isThief": False, "isNotThief": True, "t_count": 0}
+
+    return truth_dict
+
+relations = parse_logic(statements)
+# print(relations)
+
+def is_valid_solution(solution, relations):
+    for suspect, accusation in solution.items():
+        if accusation == suspect:  # A suspect cannot accuse themselves
+            return False
+        if relations[suspect]["isThief"] and accusation != relations[suspect]["subject"]:
+            return False
+        if not relations[suspect]["isThief"] and accusation == suspect:
+            return False
+    return True
+
+def find_thief_backtrack(relations, solution, i):
+    if i == len(solution):
+        if is_valid_solution(solution, relations):
+            return True
+        return False
+    
+    for suspect in relations:
+        solution[i] = suspect
+        if is_valid_solution(solution, relations) and find_thief_backtrack(relations, solution, i + 1):
+            return True
+        solution[i] = None
+    
+    return False
+
+def find_thief(relations):
+    solution = [None] * len(relations)
+    if find_thief_backtrack(relations, solution, 0):
+        return [solution[i] for i in range(len(solution))]
+    return None
+
+suspect_names, suspect_statements = generate_statements(num_suspects)
+suspect_relations = parse_logic(suspect_statements)
+thief_solution = find_thief(suspect_relations)
+
+if thief_solution:
+    print("The thief is:")
+    for i, thief in enumerate(thief_solution):
+        print(f"Suspect {suspect_names[i]} accuses {thief}")
 else:
-    print("No solution found")
-
-
-# # Test scenario
-# # suspects = ['A', 'B', 'C']
-# # statements = [
-# #     "I am not the thief",
-# #     "A is the thief",
-# #     "I am not the thief"
-# # ]
+    print("No single solution found.")
